c7b5deec3725a9753d0902f040ad64d443b423bf
Muhammad Izzatul Haq <41615982+haqizza@users.noreply.github.com>
2020-06-04 23:14:28 +0700

    Declarating local variables, creating command recognition logic, and adding error handler

---
+++ TMD/main.c
@@ -0,0 +1,37 @@
+#include "head.h"
+int main(){
+//Saya Muhammad Izzatul Haq mengerjakan evaluasi Tugas Masa Depan dalam mata kuliah Algoritma dan Pemrograman II untuk keberkahanNya maka saya tidak melakukan kecurangan seperti yang telah dispesifikasikan. Aamiin.
+    char pita[100];
+    int nPeserta = 0;
+    int nProgram = 0;
+    int nPP = 0;
+    xit = 0;
+
+    printf("Welcome to KenDB\nVersion Alpha\n\n");
+
+    while(xit == 0){
+        insertQuery(pita);
+        startKata(pita);
+
+        if(strcmp(getCKata(),"INSERT") == 0){
+            insert(pita);
+        }
+        else if(strcmp(getCKata(),"UPDATE") == 0){
+            // update(pita);
+        }
+        else if(strcmp(getCKata(),"DELETE") == 0){
+            // delete(pita);
+        }
+        else if(strcmp(getCKata(),"SELECT") == 0){
+            // select(pita);
+        }
+        else if((strcmp(getCKata(),"nice") == 0) && (jumlahKata == 4)){
+            // outCheck(pita);
+        }
+        else{
+            printf("Query tidak valid\n\n");
+        }
+    }
+
+    system("exit");
+};
